acdf2ae0a4f8b902dd8479eab7a9c12ce68be292
tariksaikiarnd <134093611+tariksaikiarnd@users.noreply.github.com>
2023-10-20 16:08:32 +0530

    Delete src directory

--- src/global/components/space/index.tsx
+++
@@ -1,9 +0,0 @@
-import "./styles/index.scss";
-interface PropTypes {
-    amt: number;
-}
-const Space = (props: PropTypes) => {
-    return <div className="space" style={{ margin: props.amt }}></div>;
-};
-
-export default Space;
